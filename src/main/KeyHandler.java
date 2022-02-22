82f245be6e1af0ac592238813cfb2ca63121a5d1
Hannah.Morken <hannah.morken@uib.no>
2022-02-22 09:13:35 +0100

    Endret tilesize og scale

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -5,7 +5,10 @@ import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener{
     
-    public boolean upPressed, downPressed, leftPressed, rightPressed;
+    public boolean upPressed,
+            downPressed,
+            leftPressed,
+            rightPressed;
 
     @Override
     public void keyTyped(KeyEvent e) {
