7d72ea5a9b4c066e7aa8eeb0b08f317f16f17d9d
Erik.G.Ingebrigtsen <erik.ingebrigtsen@student.uib.no>
2022-04-05 09:21:56 +0200

    laget klasse for ui tester

---
+++ src/test/java/uiTest/UserInterfaceTest.java
@@ -0,0 +1,27 @@
+package uiTest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserInterfaceTest {
+
+	@BeforeEach
+	void beforeEach() {
+		
+	}
+	
+	@Test
+	public void timerDrawn() {
+		
+	}
+	
+	@Test
+	public void startScreenDrawn() {
+		
+	}
+	
+	@Test
+	public void gameOverDrawn() {
+		
+	}
+}
