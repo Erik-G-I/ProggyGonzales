d264802e765e4d151df260990b9ac4f2dee1bfe2
chba <ch@L>
2022-04-19 21:25:29 +0200

    Levels menu for the maps

---
+++ src/main/java/gameState/LevelsMenu.java
@@ -0,0 +1,65 @@
+package gameState;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import core.DisplayText;
+import core.GamePanel;
+
+public class LevelsMenu extends DisplayText{
+	
+		GamePanel gp;
+		public int cmd = 1;
+		
+		public LevelsMenu(GamePanel gp) {
+			this.gp = gp;
+		}
+		
+		public void draw(Graphics g2) {
+			this.setGp(gp);
+			this.setGraphics(g2);
+			
+			String s1, s2, s3, s4, s5;
+			
+			if (gp.getLang() == Languages.NORWEGIAN) {
+				s5 = "ESC - Tilbake til hovedmeny";
+			}
+			else {
+				s5 = "ESC - Back to main menu";
+			}
+			
+			Color tranparent = new Color(0, 0, 0, 200);
+			g2.setColor(tranparent);
+			g2.fillRect(0, 0, gp.screenWidth, gp.screenHeight);
+			
+			this.draw(100);
+			s1 = "LEVELS";
+			g2.setColor(Color.BLACK);
+			g2.drawString(s1, centerText(s1)-8, gp.tileSize*2);
+			g2.setColor(Color.WHITE);
+			g2.drawString(s1, centerText(s1), gp.tileSize*2);
+			
+			this.draw(50);
+			s2 = "Map name1";
+			g2.drawString(s2, centerText(s2), gp.tileSize*5);
+			if (cmd == 1) {
+				g2.drawString(">", centerText(s2)-gp.tileSize, gp.tileSize*5);
+			}
+
+			s3 = "Map name2";
+			g2.drawString(s3, centerText(s3), gp.tileSize*6);
+			if (cmd == 2) {
+				g2.drawString(">", centerText(s3)-gp.tileSize, gp.tileSize*6);
+			}
+
+			s4 = "Map name3";
+			g2.drawString(s4, centerText(s4), gp.tileSize*7);
+			if (cmd == 3) {
+				g2.drawString(">", centerText(s4)-gp.tileSize, gp.tileSize*7);
+			}
+			
+			g2.drawString(s5, centerText(s5)-350, gp.tileSize*10);
+		}
+	}
+
+
